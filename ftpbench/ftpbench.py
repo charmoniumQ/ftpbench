972db6c52c0bf29a1dbf7fbd6333f59de2c8d9a1
Sam Grayson <sam@samgrayson.me>
2024-01-23 11:14:04 -0600

    Allow maxrun as float

--- ftpbench/ftpbench.py
+++ ftpbench/ftpbench.py
@@ -409,7 +409,7 @@ def main():
         opts["password"] = arguments["--password"]
         opts["concurrent"] = int(arguments["--concurrent"])
         opts["timeout"] = int(arguments["--timeout"])
-        opts["maxrun"] = int(arguments["--maxrun"])
+        opts["maxrun"] = float(arguments["--maxrun"])
         opts["maxiter"] = int(arguments["--maxiter"])
         opts["size"] = int(arguments["--size"])
         opts["workdir"] = arguments["<workdir>"]
